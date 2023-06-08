937eab896efcf815eec74d0b37fc981081021e69
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 04:13:12 +0100

    comp]

--- game.js
+++ game.js
@@ -1,5 +1,29 @@
-let weapons = ['rock', 'paper', 'scissors'];
+let weapons = ["rock", "paper", "scissors"];
 
-function getComputedChoice() {
-    return weapons[Math.floor(Math.random() * 10) % 3]
+function getComputerChoice() {
+  return weapons[Math.floor(Math.random() * 10) % 3];
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == computerSelection) {
+    return "Tie";
+  }
+  if (
+    (weapons.indexOf(playerSelection) + 1) % 3 ==
+    weapons.indexOf(computerSelection)
+  ) {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+  return `You win! ${playerSelection} beats ${computerSelection}`;
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt("Choose your weapon: rock / paper / scissors:");
+    let result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
+  }
+}
+
+game();
