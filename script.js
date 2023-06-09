90b53f445ee2247b2ffc82b00bfc02b66895001c
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-09 23:23:21 +0100

    pretty

--- script.js
+++ script.js
@@ -7,37 +7,36 @@ let scissors = document.querySelector(".scissors");
 let resultElement = document.querySelector(".result");
 let scoreBoard = document.querySelector(".score-board");
 function someoneWon(scores) {
-    if (scores[0] == 5) {
-        // player won
-        resultElement.textContent = "player won";
-        return true;
-    }
-    if (scores[1] == 5) {
-        // computer won
-        resultElement.textContent = "computer won"
-        return true;
-    }
-    return false;
+  if (scores[0] == 5) {
+    // player won
+    resultElement.textContent = "player won";
+    return true;
+  }
+  if (scores[1] == 5) {
+    // computer won
+    resultElement.textContent = "computer won";
+    return true;
+  }
+  return false;
 }
 
-
 function updateScore(result) {
-    let score = scoreBoard.textContent;
-    score = score.split('-');
-    if (result.split(" ").includes("win!")) {
-        score[0] = `${parseInt(score[0]) + 1}`;
-    } else if (result.split(" ").includes("lose!")) {
-        score[1] = `${parseInt(score[1]) + 1}`;
-    }
-    if (someoneWon(score)) {
-        scoreBoard.textContent = "0-0";
-        return;
-    }
-    scoreBoard.textContent = score.join("-");
+  let score = scoreBoard.textContent;
+  score = score.split("-");
+  if (result.split(" ").includes("win!")) {
+    score[0] = `${parseInt(score[0]) + 1}`;
+  } else if (result.split(" ").includes("lose!")) {
+    score[1] = `${parseInt(score[1]) + 1}`;
+  }
+  if (someoneWon(score)) {
+    scoreBoard.textContent = "0-0";
+    return;
+  }
+  scoreBoard.textContent = score.join("-");
 }
 
 function playGame(userChoice) {
-  let result =  playRound(userChoice, getComputerChoice());
+  let result = playRound(userChoice, getComputerChoice());
   resultElement.textContent = result;
   updateScore(result);
 }
